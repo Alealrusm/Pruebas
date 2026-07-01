d6524bf315fa063b3b1b14c0740579cbc45f2ca4
Alealrusm <alealr@usm.cl>
2026-07-01 13:26:04 -0400

    Limpiar

--- scripts/combat/defend_skill.gd
+++
@@ -1,5 +0,0 @@
-class_name DefendSkill extends Skill
-
-func ejecutar(usuario: Entity, objetivo: Entity) -> String:
-	usuario.defensa += poder
-	return "%s se defiende (+%d defensa)" % [usuario.nombre, poder]
--- scripts/combat/healt_skill.gd
+++
@@ -1,5 +0,0 @@
-class_name HealSkill extends Skill
-
-func ejecutar(usuario: Entity, objetivo: Entity) -> String:
-	usuario.curar(poder)
-	return "%s se cura %d HP" % [usuario.nombre, poder]
